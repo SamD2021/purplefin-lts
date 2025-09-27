a0d8513dd4d2cf9a001c3a61493de5b48aa7d3f3
Sam Dasilva <samuelramos852@gmail.com>
2025-09-26 20:41:09 -0400

    feat(cosmic): Add Cosmic DE from EPEL Copr
    Signed-off-by: Sam Dasilva <samuelramos852@gmail.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf copr enable -y ligenix/enterprise-cosmic rhel+epel-10-x86_64
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf install -y \
+  cosmic-desktop \
+  tmux \
+  && dnf clean all
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf copr disable ligenix/enterprise-cosmic
 
-#### Example for enabling a System Unit File
 
 systemctl enable podman.socket
