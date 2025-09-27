4709d78ecd39b3603717bfd78dd50a7d6e004541
Sam Dasilva <samuelramos852@gmail.com>
2025-09-27 12:42:59 -0400

    feat(portal): Add point more desktop functionality to gnome
    Signed-off-by: Sam Dasilva <samuelramos852@gmail.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,14 +5,27 @@ mkdir -p /etc/xdg-desktop-portal
 
 tee /etc/xdg-desktop-portal/portals.conf >/dev/null <<'EOF'
 [preferred]
-# use GNOME/GTK backends by default
+# Default: GNOME first, then GTK fallback
 default=gnome;gtk;
 
-# be explicit for common interfaces
+# Explicit mappings
 org.freedesktop.impl.portal.FileChooser=gnome;gtk;
 org.freedesktop.impl.portal.Secret=gnome-keyring;gnome;gtk;
 org.freedesktop.impl.portal.Screenshot=gnome;gtk;
 org.freedesktop.impl.portal.Wallpaper=gnome;gtk;
+
+# Screen sharing / remote desktop
+org.freedesktop.impl.portal.ScreenCast=gnome;gtk;
+org.freedesktop.impl.portal.RemoteDesktop=gnome;gtk;
+
+# Notifications
+org.freedesktop.impl.portal.Notification=gnome;gtk;
+
+# Background apps
+org.freedesktop.impl.portal.Background=gnome;gtk;
+
+# Location (if needed)
+org.freedesktop.impl.portal.Location=gnome;gtk;
 EOF
 
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
